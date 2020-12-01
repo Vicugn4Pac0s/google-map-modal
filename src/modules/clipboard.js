965fe3d490c698834204cf6b4366befddd1ff144
YOSHIKI <vicugnapacos.g@gmail.com>
2020-12-01 22:13:23 +0900

    コピー失敗問題　修正

--- src/modules/clipboard.js
+++ src/modules/clipboard.js
@@ -16,8 +16,7 @@ export default class {
     let txt = this.$_txtCopy.innerText;
     navigator.clipboard
       .writeText(txt)
-      .then((data) => alert("コピーしました。"))
-      .catch((e) => alert("コピーに失敗しました。。。ブラウザが対応していないのかもしれません。お手数ですが、直接コピーしてください。"));
+      .then((data) => alert("コピーしました。"), (e) => alert("コピーに失敗しました。。。ブラウザが対応していないのかもしれません。お手数ですが、直接コピーしてください。"))
   }
   active() {
     if (this.state === 1 || !this.is_use_clipboard) return false;
